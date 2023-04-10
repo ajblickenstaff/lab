5566665163e772f003e3d73c4dc5fff1e8dcbf23
Anthony Blickenstaff <30305205+ajblickenstaff@users.noreply.github.com>
2023-04-10 12:31:29 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,24 +1,51 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Initialize the Account object with a given name and a balance of 0.
+
+        :param name: The name of the account holder.
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Deposit a specified amount into the account.
+
+        :param amount: The amount to be deposited.
+        :return: True if the deposit transaction is successful, False otherwise.
+        """
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Withdraw a specified amount from the account.
+
+        :param amount: The amount to be withdrawn.
+        :return: True if the withdrawal transaction is successful, False otherwise.
+        """
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Get the account balance.
+
+        :return: The account balance.
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Get the account name.
+
+        :return: The account name.
+        """
         return self.__account_name
